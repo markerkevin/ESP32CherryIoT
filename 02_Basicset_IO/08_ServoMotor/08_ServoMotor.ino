aff09647df97396afc7f4df76461dc3d6502c4dd
markerkevin <markerk53@gmail.com>
2025-05-22 15:45:05 +0900

    Change angle

--- 02_Basicset_IO/08_ServoMotor/08_ServoMotor.ino
+++ 02_Basicset_IO/08_ServoMotor/08_ServoMotor.ino
@@ -7,16 +7,35 @@ Servo myservo;
 void setup() {
   myservo.attach(motorPin);
 }
-
 void loop() {
   // 0-180 right
-  for(int angle = 0; angle <= 180; angle++) {                        
+  for(int angle = 0; angle <= 90; angle+=10) {                        
     myservo.write(angle);
-    delay(15);
+    delay(100);
   }
-  // 180-0  left 
-  for(int angle = 180; angle >= 0; angle--) {                 
-    myservo.write(angle);  
-    delay(15);      
+  for(int angle = 15; angle <= 105; angle+=10) {                        
+    myservo.write(angle);
+    delay(100);
+  }
+  for(int angle = 30; angle <= 120; angle+=10) {                        
+    myservo.write(angle);
+    delay(100);
+  }
+  for(int angle = 45; angle <= 135; angle+=10) {                        
+    myservo.write(angle);
+    delay(100);
+  }
+  for(int angle = 60; angle <= 150; angle+=10) {                        
+    myservo.write(angle);
+    delay(100);
+  }
+  for(int angle = 75; angle <= 165; angle+=10) {                        
+    myservo.write(angle);
+    delay(100);
+  }
+  for(int angle = 90; angle <= 180; angle+=10) {                        
+    myservo.write(angle);
+    delay(100);
   }
 }
+
