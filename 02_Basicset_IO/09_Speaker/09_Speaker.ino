e61dcc60361389a436e1e9ad140a5861245791eb
markerkevin <markerk53@gmail.com>
2025-05-29 13:42:03 +0900

    Original melody

--- 02_Basicset_IO/09_Speaker/09_Speaker.ino
+++ 02_Basicset_IO/09_Speaker/09_Speaker.ino
@@ -12,25 +12,139 @@ const int spkrPin = 3; //3:ConnectorA 4:ConnectorB
 #define RA 440
 #define _RA 466.164
 #define TI 493.883
-#define octDO 523.251
+#define DO_oct 523.251
+#define BEAT 250  // 0.25秒のテンポ
 
-void playmusic(){
+
+
+void arupusu(){
   ledcWriteTone(spkrPin, DO);
-  delay(250);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO);
+  delay(BEAT);
   ledcWriteTone(spkrPin, RE);
-  delay(250);
+  delay(BEAT);
   ledcWriteTone(spkrPin, MI);
-  delay(250);
-  ledcWriteTone(spkrPin, 0); // no sound
-  delay(250);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, SO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RE);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RE);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT * 3);
+
+
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, FA);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, SO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO_oct);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RA);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO_oct);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, SO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT * 3);
+
+
+  ledcWriteTone(spkrPin, DO_oct);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO_oct);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, TI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, SO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RA);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RA);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RA);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RA);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, SO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT * 3);
+
+
+  ledcWriteTone(spkrPin, DO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RE);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, SO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, MI);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RE);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, RE);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, DO);
+  delay(BEAT);
+  ledcWriteTone(spkrPin, 0);
+  delay(BEAT * 3);
+}
+
+void loop() {
+  // 1回だけ再生
+  while(true);
 }
 
 void setup() {
   pinMode(spkrPin, OUTPUT);
   ledcAttach(spkrPin, 12000, 8); //Pin setting(Pin num, Max frequency, Resolution)
-}
-
-void loop() {
-  playmusic();
-  delay(3000);
+  arupusu();
 }
